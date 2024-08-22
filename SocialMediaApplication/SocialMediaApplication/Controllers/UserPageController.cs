d28fdabf1ec40540641aed54d3d2560ac901e235
Shuting Tang <tangst816@gmail.com>
2024-08-22 01:02:59 -0400

    Template Upload
    Create project.
    Add HTML/CSS styles as components template in User Page

---
+++ SocialMediaApplication/SocialMediaApplication/Controllers/UserPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaApplication.Controllers
+{
+    public class UserPageController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SocialMediaApplication/SocialMediaApplication/ViewComponents/PostViewComponent.cs
@@ -0,0 +1,6 @@
+﻿namespace SocialMediaApplication.ViewComponents
+{
+    public class PostViewComponent
+    {
+    }
+}
