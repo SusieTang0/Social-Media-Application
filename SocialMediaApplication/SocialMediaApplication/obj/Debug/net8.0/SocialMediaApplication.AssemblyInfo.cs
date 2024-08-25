a4aca8824043bd9ad5a1aa1dcd181eea0ba1c21d
Fatima Fekri <sffekri@gmail.com>
2024-08-25 12:08:46 -0400

    Change file path when first created the branch

--- SocialMediaApplication/SocialMediaApplication/obj/Debug/net8.0/SocialMediaApplication.AssemblyInfo.cs
+++ SocialMediaApplication/SocialMediaApplication/obj/Debug/net8.0/SocialMediaApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28fdabf1ec40540641aed54d3d2560ac901e235")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31aade3991d404ff407c613d4eacdc08ccd9fa30")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
