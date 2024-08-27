8349cb9130901b18647c9b0d26335d87435b1cc3
Shuting Tang <tangst816@gmail.com>
2024-08-27 00:49:07 -0400

    Update part of post feature
    1.show the post info on site
    2. update the models and layouts

--- SocialMediaApplication/SocialMediaApplication/Models/User.cs
+++ SocialMediaApplication/SocialMediaApplication/Models/User.cs
@@ -5,6 +5,7 @@ namespace SocialMediaApplication.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Description { get; set; }
         public string AvatarURL { get; set; } = "";
         public DateTime CreatedTime { get; set; }
     }
